a09d484ca0e7f3a66ca6be79badb4ba2404a31bc
sax <s.apostolico@gmail.com>
2018-05-23 19:18:31 +0200

    updates

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,7 @@ set -ex
 #
 
 export PGDATA=/var/lib/postgresql/9.6/main
+mkdir -p /docker-entrypoint.d
 
 mkdir -p "$PGDATA"
 chown -R postgres "$PGDATA" 2>/dev/null || :
@@ -13,6 +14,19 @@ if [ ! -f "$PGDATA/PG_VERSION" ]; then
 fi
 
 service postgresql start
+
+echo
+for f in /docker-entrypoint.d/*; do
+    case "$f" in
+        *.py)     echo "$0: running $f"; "/bin/bash $f" ;;
+        *.sh)     echo "$0: running $f"; . "$f" ;;
+        *.sql)    echo "$0: running $f"; "${psql[@]}" -f "$f"; echo ;;
+        *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+        *)        echo "$0: ignoring $f" ;;
+    esac
+    echo
+done
+
 
 if [ -n "$POSTGRES_DB" ]; then
     createdb --username postgres "$POSTGRES_DB" 2>/dev/null || :
